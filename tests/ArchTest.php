0dacfd0271f5efcafa6c6c61759b52046b5a876b
Joey McKenzie <joey.mckenzie27@gmail.com>
2024-06-24 13:17:22 -0700

    chore: update for php package, remove laravel deps

--- tests/ArchTest.php
+++ tests/ArchTest.php
@@ -1,5 +1,13 @@
 <?php
 
-arch('it will not use debugging functions')
+arch('It will not use debugging functions')
     ->expect(['dd', 'dump', 'ray'])
     ->each->not->toBeUsed();
+
+arch('All source files are strictly typed')
+    ->expect('VendorName\\Skeleton\\')
+    ->toUseStrictTypes();
+
+arch('All tests files are strictly typed')
+    ->expect('VendorName\\Skeleton\\Tests\\')
+    ->toUseStrictTypes();

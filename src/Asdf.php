f7f158371403bad266dac7ca39ebae113a1713ae
Joey McKenzie <joey.mckenzie27@gmail.com>
2024-06-26 09:43:40 -0700

    chore: cleanup

--- src/Asdf.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-namespace Asdf\Asdf;
-
-final class Asdf {}
--- src/Screamforce.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-namespace Testvendor\Screamforce;
-
-final class Screamforce {}
